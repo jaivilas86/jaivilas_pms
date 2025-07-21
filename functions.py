9d0d20a375ce57b3a322960eca088f6b470aca07
Jaivilas <dme2@jaivilas.in>
2025-07-21 17:36:47 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import re
 def extract_all_areas(text):
     # Sabhi (Block + Number) patterns निकालेगा, जैसे A1802, B602, D1102, B803
     matches = re.findall(r'([A-D])[- ]?(\d{3,4})', text, re.IGNORECASE)
